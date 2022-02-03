e92bb8b787a1368c0934f9be1de6ec58effae578
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-03 02:01:08 +0100

    added request get and delete user

--- src/main/java/com/microservices/microservices/controller/UserController.java
+++ src/main/java/com/microservices/microservices/controller/UserController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,4 +37,15 @@ public class UserController {
 	public ResponseEntity<User> create(@RequestBody User user){
 		return new ResponseEntity<User>(userService.create(user), HttpStatus.CREATED);
 	}
+	
+	@GetMapping( value = "/get/{id}", produces = "application/json")
+	public ResponseEntity<User> getUser(@PathVariable("id") Long id){
+		return new ResponseEntity<User>( userService.getUser(id), HttpStatus.OK);
+	}
+	
+	@DeleteMapping( value = "/delete/{id}", produces = "application/json")
+	public ResponseEntity<User> delete(@PathVariable("id") Long id){
+		userService.delete(id);
+		return new ResponseEntity<User>(HttpStatus.OK);
+	} 
 }
