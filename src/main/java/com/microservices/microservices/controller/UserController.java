7b21a28ca6904194d2abd3ac48a603e5b30b4aa4
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-03 01:50:41 +0100

    added user request

--- src/main/java/com/microservices/microservices/controller/UserController.java
+++ src/main/java/com/microservices/microservices/controller/UserController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +24,15 @@ public class UserController {
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
-	
-	@ResponseBody
+
 	@GetMapping(value = "/all", produces = "application/json")
 	public ResponseEntity<List<User>> readAll(){
 		List userList = userService.readAll();
 		return new ResponseEntity<List<User>>(userList, HttpStatus.OK);
 	}   
+	
+	@PostMapping( value = "/add", produces = "application/json")
+	public ResponseEntity<User> create(@RequestBody User user){
+		return new ResponseEntity<User>(userService.create(user), HttpStatus.CREATED);
+	}
 }
