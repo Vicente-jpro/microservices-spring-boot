db29f25c703664ab7e9dff6dac1bc3f81c31f2b5
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-02-24 00:02:21 +0100

    Join table users_role added

--- src/main/java/com/microservices/microservices/domain/User.java
+++ src/main/java/com/microservices/microservices/domain/User.java
@@ -1,19 +1,24 @@
 package com.microservices.microservices.domain;
 
-import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
+import org.hibernate.annotations.ForeignKey;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -49,6 +54,13 @@ public class User implements UserDetails{
 	@OneToMany( mappedBy = "user", cascade = CascadeType.ALL)
 	private List<Telephone> telephones ;
 
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable( name = "users_role", 
+	uniqueConstraints = @UniqueConstraint( columnNames = {"user_id", "role_id"}, name = "unique_role_user"),
+	joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id", table = "user"),
+	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role")
+	)
+	private List<Role> roles;
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return null;
